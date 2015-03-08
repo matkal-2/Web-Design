a28883162b429bf07267c1f88b3f5fd654e7e051
matkal-2 <matkal-2@student.ltu.se>
2015-03-08 23:22:14 +0100

    more about me

--- kallsaby_se/app/views/mattias/home/index.php
+++ kallsaby_se/app/views/mattias/home/index.php
@@ -6,6 +6,13 @@ include("php/menu_bar.php");
 echo '
 	<div class="body-background">
 		<div class="body-content">
+		<div class="body-center-container">
+			<h1>V&auml;lkommen!</h1>
+		</div>
+		<div class="index-top-pic">
+			<img src="/img/cheers.jpg" alt="Profile Picture">
+		</div>
+			
 		</div>
 	</div>';
 readfile("html/foot_bar.html");
