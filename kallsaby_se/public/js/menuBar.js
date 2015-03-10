cc4243f7b4f71b365bb82a902993b8a60f32ea64
matkal-2 <matkal-2@student.ltu.se>
2015-03-10 18:32:41 +0100

    compatability

--- kallsaby_se/public/js/menuBar.js
+++ kallsaby_se/public/js/menuBar.js
@@ -1,10 +1,16 @@
-function dropDown(target){
+function dropDown(target){	
+	document.getElementById(target).style.Transition = "max-height 0.8s";
 	document.getElementById(target).style.WebkitTransition = "max-height 0.8s";
+	document.getElementById(target).style.MozTransition = "max-height 0.8s";
+	document.getElementById(target).style.OTransition = "max-height 0.8s";
 	document.getElementById(target).style.maxHeight = "400px";
 
 }
 
 function riseUp(target){
+	document.getElementById(target).style.Transition = "max-height 0.2s";
 	document.getElementById(target).style.WebkitTransition = "max-height 0.2s";
+	document.getElementById(target).style.MozTransition = "max-height 0.2s";
+	document.getElementById(target).style.OTransition = "max-height 0.2s";
 	document.getElementById(target).style.maxHeight = "0px";
 }
