0486f4157f61480273fc6b949f1e4cb4971028bd
matkal-2 <matkal-2@student.ltu.se>
2015-03-12 18:06:10 +0100

    comp

--- kallsaby_se/public/js/menuBar.js
+++ kallsaby_se/public/js/menuBar.js
@@ -3,6 +3,7 @@ function dropDown(target){
 	document.getElementById(target).style.WebkitTransition = "max-height 0.8s";
 	document.getElementById(target).style.MozTransition = "max-height 0.8s";
 	document.getElementById(target).style.OTransition = "max-height 0.8s";
+	document.getElementById(target).style.msTransition = "max-height 0.8s";
 	document.getElementById(target).style.maxHeight = "400px";
 
 }
@@ -12,5 +13,6 @@ function riseUp(target){
 	document.getElementById(target).style.WebkitTransition = "max-height 0.2s";
 	document.getElementById(target).style.MozTransition = "max-height 0.2s";
 	document.getElementById(target).style.OTransition = "max-height 0.2s";
+	document.getElementById(target).style.msTransition = "max-height 0.2s";
 	document.getElementById(target).style.maxHeight = "0px";
 }
--- kallsaby_se/public/js/slider.js
+++ kallsaby_se/public/js/slider.js
@@ -5,10 +5,12 @@ function slide(pos, target){
 	document.getElementById(target).style.WebkitTransitionTimingFunction = "cubic-bezier(0,0,0,1)";
 	document.getElementById(target).style.MozTransitionTimingFunction = "cubic-bezier(0,0,0,1)";
 	document.getElementById(target).style.OTransitionTimingFunction = "cubic-bezier(0,0,0,1)";
+	document.getElementById(target).style.msTransitionTimingFunction = "cubic-bezier(0,0,0,1)";
 	document.getElementById(target).style.Transition = "margin 1s";
 	document.getElementById(target).style.WebkitTransition = "margin 1s";
 	document.getElementById(target).style.MozTransition = "margin 1s";
 	document.getElementById(target).style.OTransition = "margin 1s";
+	document.getElementById(target).style.msTransition = "margin 1s";
 	document.getElementById(target).style.marginLeft = posi;
 
 }
