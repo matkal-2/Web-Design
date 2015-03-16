76c62092f2540df9a19544c30ec00f5a5b2fd573
matkal-2 <matkal-2@student.ltu.se>
2015-03-16 16:40:20 +0100

    Removed back-end on fam site

--- kallsaby_se/app/controllers/home.php
+++ kallsaby_se/app/controllers/home.php
@@ -2,37 +2,6 @@
 
 class Home extends Controller{
 	public function index(){
-		session_start();
-		if(isset($_GET['logOut'])){
-			session_destroy();
-			header( 'Location: /login/index' ) ;
-		}
-		$visitor = $this->model('Visitor');
-
-		$this->view('home/index', ['color_theme' => $this->getColorTheme(), 'logged_in' => $visitor->isLoggedIn()]);
-	}
-	public function about(){
-		session_start();
-
-		$visitor = $this->model('Visitor');
-
-		$this->view('home/about', ['color_theme' => $this->getColorTheme(), 'logged_in' => $visitor->isLoggedIn()]);
-	}
-
-	public function aboutCont(){
-		if(isset($_POST['pos'])){				
-			$visitor = $this->model('Visitor');
-			$this->view('home/aboutCont', ['color_theme' => $this->getColorTheme(), 'logged_in' => $visitor->isLoggedIn()]);
-		}else{
-			header( 'Location: /home/about' );
-		}
-	}
-
-	public function cv(){
-		session_start();
-
-		$visitor = $this->model('Visitor');
-
-		$this->view('home/cv', ['color_theme' => $this->getColorTheme(), 'logged_in' => $visitor->isLoggedIn()]);
+		$this->view('home/index');
 	}
 }
