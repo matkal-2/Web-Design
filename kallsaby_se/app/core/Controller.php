2033a690aa1d9d975641657660de44a256f89ce7
matkal-2 <matkal-2@student.ltu.se>
2015-03-16 18:04:31 +0100

    echo

--- kallsaby_se/app/core/Controller.php
+++ kallsaby_se/app/core/Controller.php
@@ -14,17 +14,24 @@ class Controller{
 	}
 
 	public function view($layout, $view, $data = []){
+		echo "heyo";
 		require_once '../app/lib/viewHelper.php';
+		echo "heyo 1";
 		$vh = new viewHelper();
+		echo "heyo 2";
 		$js = $vh->importJS($view);
-		
+		echo "heyo 3";
+
 		$expView = explode('/',$view , FILTER_SANITIZE_URL));
+		echo "heyo 4 ";
 		$style = '<link rel="stylesheet" href="/css/fam.css">';
 		if(file_exists('/public/css/'.$styler[0].'.css')){
 			$style .= '<link rel="stylesheet" href="/css/'.$expView[0].'.css">';
 		}
+		echo "heyo 5";
 		
 		require_once '../app/views/layouts/'.$layout.'.phtml';
+		echo "heyo 6";
 	}
 
 }
